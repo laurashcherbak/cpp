6321d017c5956cfa1ccd8f1cd42a91cc43536582
Laura Shcherbak <laura.shcherbak@gmail.com>
2020-11-16 14:13:59 +0200

    Lab6.2.1 added

---
+++ pr6.2/pr6.2.1/UnitTest/UnitTest.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../pr6.2.1/pr6.2.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestArrays)
+		{
+			const int n = 10;
+			int a[n] = { -6, 2, -5, 6, 3, 4, 3, 5, 7, 10 };
+			int r[n] = { 10, 2, -5, 6, 3, 4, 3, 5, 7, -6 };
+
+			SwapMinMaxInArray(a, n);
+
+			for (int i = 0; i < n; i++) {
+				Assert::AreEqual(r[i], a[i]);
+			}
+		}
+	};
+}
