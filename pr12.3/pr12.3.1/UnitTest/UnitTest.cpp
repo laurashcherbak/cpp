14f7f9b23073cfdeee9301405db23e45e236772f
Shcherbak Laura <laura.shcherbak@gmail.com>
2020-12-22 00:23:03 +0200

    Lab 12.3 added

---
+++ pr12.3/pr12.3.1/UnitTest/UnitTest.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab/Lab.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestEnqueueAndProcess)
+		{
+			Elem* first = NULL;
+			Elem* last = NULL;
+			for (int i = 0; i < 3; i++)
+				Enqueue(first, last, i + 1);
+
+			int V1 = 3;
+			int V2 = 4;
+			Process(first, V1, V2);
+
+			for (int i = 0; i < 4; i++) {
+				Assert::AreEqual(i + 1, first->info);
+				first = first->next;
+			}
+
+		}
+	};
+}
